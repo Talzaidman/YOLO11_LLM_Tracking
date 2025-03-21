ca2f2dcf669be14336c2348b633f4e786f3d1950
Talzaidman <zai.tal88@gmail.com>
2025-03-21 11:52:24 +0200

    commit with the new camera

--- Ask_yolo.py
+++ Ask_yolo.py
@@ -1,10 +1,9 @@
 import cv2
 from Cropped2center import cropped2center
 
-cam_width, cam_height = 1000, 1000
-STRIDE = 5
+cam_width, cam_height = 1388, 1038
 
-def asking_yolo(model, frame, chosen_class, past_coor, frame_num):
+def asking_yolo(model, frame, chosen_class):
     target_found = False
     height, width, channels = frame.shape
     results = model(frame, imgsz=max(height, width))
@@ -12,19 +11,9 @@ def asking_yolo(model, frame, chosen_class, past_coor, frame_num):
         for box in result.boxes.data:  # Iterate over detected boxes
             # Extract bounding box information
             x1, y1, x2, y2, conf, cls = box.cpu().numpy()  # Convert to NumPy array
-
             # Check if the detected class matches the target class
             if result.names[int(cls)] == chosen_class:
-                # Ensure bounding box coordinates are within frame dimensions
-                x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])
-                x1, y1 = max(0, x1), max(0, y1)
-                x2, y2 = min(frame.shape[1], x2), min(frame.shape[0], y2)
-
-                # Crop the frame based on the bounding box
-                cropped = frame[y1:y2, x1:x2]
-                cv2.imshow("Full Frame", cropped2center(cropped))
-
                 target_found = True
                 return results, x1, y1, x2, y2, target_found
-    x1, y1, x2, y2 = None, None, None, None
+    x1, y1, x2, y2 = 0, 0, cam_width, cam_height
     return results, x1, y1, x2, y2, target_found
--- maincode.py
+++ maincode.py
@@ -9,66 +9,55 @@ from vimba import *
 from Cropped2center import cropped2center
 
 # Load a model
-model = YOLO("yolo11n.pt")
-cam_width = 1000
-cam_height = 1000
-STRIDE = 5
+model = YOLO("yolo11s.pt")
+cam_width = 1388
+cam_height = 1038
+STRIDE = 15
 target_found = False
 padding = 30
 fps_ls = []
 x1, y1, x2, y2 = 0, 0, 0, 0
 chosen_class = "person"
-
+delta_x1 = 0
+delta_y1 = 0
 
 # Open the default camera (0 for the first camera, 1 for the second, etc.)
 # cap = cv2.VideoCapture(0)
 
-
-# print(cam.get_feature_by_name.__doc__)
 def frame_handler(camera, frame):
-    global target_found, x1, y1, x2, y2, chosen_class, fps_ls
+    global target_found, x1, y1, x2, y2, chosen_class, fps_ls, delta_x1, delta_y1
     camera.queue_frame(frame)
     start_time = time.time()
     numpy_array = frame.as_numpy_ndarray()
     frame = cv2.cvtColor(numpy_array, cv2.COLOR_BayerRG2BGR)
+    frame2show = cropped2center(frame)
+    frame_small = cv2.resize(frame2show, (frame2show.shape[1] // 2, frame2show.shape[0] // 2))
+    cv2.imshow("Full Frame", frame_small)
 
     # ret, frame = cap.read()
     if len(fps_ls) % STRIDE == 0:
+        target_found = False
+
+        results, x1, y1, x2, y2, target_found = asking_yolo(model,frame,chosen_class)
+
         if target_found:
-            target_found = False
-            # Store the original coordinates for coordinate transformation
-            crop_x1 = max(0, x1 - padding)
-            crop_y1 = max(0, y1 - padding)
-
-            # Crop the frame
-            cropped_frame = frame[max(0, y1 - padding): min(frame.shape[0], y2 + padding),
-                            max(0, x1 - padding): min(frame.shape[1], x2 + padding)]
-
-            # Process the cropped frame
-            results, rel_x1, rel_y1, rel_x2, rel_y2, target_found = asking_yolo(model,
-                                                                                cropped_frame,
-                                                                                chosen_class, [x1, y1, x2, y2],
-                                                                                len(fps_ls))
-
-            # If object found in cropped frame, transform coordinates back to full frame
-            if target_found:
-                x1 = rel_x1 + crop_x1
-                y1 = rel_y1 + crop_y1
-                x2 = rel_x2 + crop_x1
-                y2 = rel_y2 + crop_y1
-
-        if not target_found:
-            target_found = False
-            results, x1, y1, x2, y2, target_found = asking_yolo(model,
-                                                                frame,
-                                                                chosen_class, [x1, y1, x2, y2], len(fps_ls))
-            # Display the full annotated frame if no target class is found
-
-            annotated_frame = results[0].plot()
-            cv2.imshow("Full Frame", annotated_frame)
-    else:
-        cropped = frame[y1:y2, x1:x2]
-        cv2.imshow("Full Frame", cropped2center(cropped))
+            x1 += delta_x1
+            y1 += delta_y1
+            x2 += delta_x1
+            y2 += delta_y1
+            cam.get_feature_by_name("Height").set(min(y2 - y1 + 2*padding,cam_height- max(0, y1 - padding)))
+            cam.get_feature_by_name("Width").set(min(x2-x1 + 2*padding,cam_width- max(0, x1 - padding)))
+            cam.get_feature_by_name("OffsetY").set(max(0, y1 - padding))
+            cam.get_feature_by_name("OffsetX").set(max(0, x1 - padding))
+            delta_x1 = max(0, x1 - padding)
+            delta_y1 = max(0, y1 - padding)
+        else:
+            cam.get_feature_by_name("Height").set(cam_height)
+            cam.get_feature_by_name("Width").set(cam_width)
+            cam.get_feature_by_name("OffsetY").set(0)
+            cam.get_feature_by_name("OffsetX").set(0)
+            delta_x1 = 0
+            delta_y1 = 0
 
     # Press 'q' to quit or 'c' to change class
     key = cv2.waitKey(1) & 0xFF
